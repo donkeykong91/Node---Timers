0e8bd52f3931a570deec1674c17b228654d5c5ee
donkeykong91 <darylnator@gmail.com>
2018-06-29 02:55:10 -0700

    Adding setInterval to show elapsed time

--- timers.js
+++ timers.js
@@ -1,6 +1,14 @@
 var waitTime = 3000;
+var currentTime = 0;
+var waitInterval = 500;
 
 console.log("Wait for it");
+
+setInterval(function () {
+  currentTime += waitInterval;
+
+  console.log(`waiting ${currentTime/1000} seconds...`);
+}, waitInterval);
 
 setTimeout(function () {
   console.log("done");
